db2c8b7df797ed6c4aa125aba837695d497d6f82
devinwelch <devinmwelch@gmail.com>
2017-12-06 23:35:48 -0500

    eased test messages

--- bot.js
+++ bot.js
@@ -8,30 +8,28 @@ client.on("ready", () => {
 });
 
 client.on("message", message => {
-    message.channel.send("test1");
     if (message.author.bot) return;
     if (message.content[0] === '!') {
         cmd = message.content.split(/[\s!]+/)[1].toLowerCase();
         params = message.content.substring(cmd.length + 1, message.content.length).trim();
-        message.channel.send("test2");
         switch(cmd) {
             case "nominate":
                 album = params.split(" - ")[0];
                 artist = params.split(" - ")[1];
-                message.channel.send("test3");
                 sql.get(`SELECT * FROM database`).then(row => {
                     sql.run("INSERT INTO albums (album, artist, userID) VALUES (?, ?, ?)", [album, artist, message.author.id]);
+                    message.channel.send("success");
                     console.log(message.author.id + " added " + params);
                     message.channel.send(message.author.id + " added " + params);
                 }).catch(() => {
                     console.error;
                     sql.run("CREATE TABLE IF NOT EXISTS albums (album TEXT, artist TEXT, userID TEXT)").then(() => {
+                        message.channel.send("failure");
                         sql.run("INSERT INTO albums (album, artist, userID) VALUES (?, ?, ?)", [album, artist, message.author.id]);
                     });
                 });
                 break;
             default:
-                message.channel.send("test4");
                 break;
         }
     }
