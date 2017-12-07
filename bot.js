b5dab6095798b5223b413c0144488d939120395c
devinwelch <devinmwelch@gmail.com>
2017-12-06 23:14:24 -0500

    logging

--- bot.js
+++ bot.js
@@ -10,14 +10,16 @@ client.on("ready", () => {
 client.on("message", message => {
     if (message.author.bot) return;
     if (message.content[0] === '!') {
-        cmd = message.content.split(/[\s!]+/)[1];
-        params = message.content.substring(cmd.length + 1, message.content.length)
+        cmd = message.content.split(/[\s!]+/)[1].toLowerCase();
+        params = message.content.substring(cmd.length + 1, message.content.length).trim();
         switch(cmd) {
             case "nominate":
                 album = params.split(" - ")[0];
                 artist = params.split(" - ")[1];
                 sql.get(`SELECT * FROM database`).then(row => {
                     sql.run("INSERT INTO albums (album, artist, userID) VALUES (?, ?, ?)", [album, artist, message.author.id]);
+                    console.log(message.author.id + " added " + params);
+                    message.channel.send(message.author.id + " added " + params);
                 }).catch(() => {
                     console.error;
                     sql.run("CREATE TABLE IF NOT EXISTS albums (album TEXT, artist TEXT, userID TEXT)").then(() => {
