539e31f31098b06b9f5a86735f37a5caf6a14bd2
Devin <devinmwelch@gmail.com>
2019-06-17 19:33:17 -0400

    test

--- bot.js
+++ bot.js
@@ -1,13 +1,19 @@
-const Discord = require("discord.js");
-const client = new Discord.Client();
-const sql = require("sqlite")
-sql.open("./albums.sqlite");
+const Discord = require("discord.js")
+const client = new Discord.Client()
+//const sql = require("sqlite")
+//sql.open("./albums.sqlite");
 
 client.on("ready", () => {
-    console.log("I am ready!");
-});
+    console.log("I am ready!")
+})
 
 client.on("message", message => {
+    if (message.content === "hi") {
+        message.reply('hello world')
+    }
+})
+
+/*client.on("message", message => {
     if (message.author.bot) return;
     if (message.content[0] === '!') {
         cmd = message.content.split(/[\s!]+/)[1].toLowerCase();
@@ -16,15 +22,16 @@ client.on("message", message => {
             case "nominate":
                 album = params.split(" - ")[0];
                 artist = params.split(" - ")[1];
+                message.channel.send("stuff:" + album, artist, message.author.id); //test
                 sql.get(`SELECT * FROM database`).then(row => {
                     sql.run("INSERT INTO albums (album, artist, userID) VALUES (?, ?, ?)", [album, artist, message.author.id]);
-                    message.channel.send("success");
+                    message.channel.send("success"); //test
                     console.log(message.author.id + " added " + params);
                     message.channel.send(message.author.id + " added " + params);
                 }).catch(() => {
                     console.error;
                     sql.run("CREATE TABLE IF NOT EXISTS albums (album TEXT, artist TEXT, userID TEXT)").then(() => {
-                        message.channel.send("failure");
+                        message.channel.send("failure"); //test
                         sql.run("INSERT INTO albums (album, artist, userID) VALUES (?, ?, ?)", [album, artist, message.author.id]);
                     });
                 });
@@ -33,6 +40,6 @@ client.on("message", message => {
                 break;
         }
     }
-});
+});*/
 
 client.login(process.env.BOT_TOKEN);
